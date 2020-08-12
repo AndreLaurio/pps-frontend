5a9fb7a2e3997c0a19a2013422680f8c1aac87da
Hyperstorm321 <45072321+Hyperstorm321@users.noreply.github.com>
2020-08-12 14:53:01 -0800

    FIRST UPLOAD

---
+++ src/components/Register.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <h1></h1>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        
+    }
+}
+</script>
