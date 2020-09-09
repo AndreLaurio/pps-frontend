511c64d77cd8b37c6bb5ed03fc4a1b1252c5eead
Andre Laurio <45072321+Hyperstorm321@users.noreply.github.com>
2020-09-10 00:34:47 +0800

    [STAGING] User Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,18 +1,55 @@
 <template>
-    <div class="pop mt-12 ml-12">
+    <div class="pop mt-5 ml-12">
         <h2 class="mb-12">Profile</h2>
         <v-layout justify-center>
-            <v-flex xs12 sm12 md10 xl3>
+            <v-flex xs12 sm12 md3 xl3>
                 sample image
             </v-flex>
-            <v-flex xs12 sm12 md10 xl6>
+            <v-flex xs12 sm12 md8 xl6>
                 <v-card class="rounded-xl" elevation="5">
                     <v-card-title>
-                        Details
+                        <span class="ml-5 mt-5">Details</span> 
                     </v-card-title>
-                    <v-card-text>
-                        <label for="fname">First name:</label><br>
-                          <v-text-field label="Another input"></v-text-field>
+                    <v-card-text class="ml-7">
+                        <v-layout>
+                            <v-flex>
+                                First Name<v-text-field solo dense v-model="user_details.first_name"></v-text-field>
+                                Middle Name<v-text-field solo dense v-model="user_details.middle_name"></v-text-field>
+                                Address<v-text-field solo dense v-model="user_details.address"></v-text-field>
+                            </v-flex>
+                            <v-flex>
+                                Last Name<v-text-field solo dense v-model="user_details.last_name"></v-text-field>
+                                Extension Name<v-text-field solo dense v-model="user_details.extension_name"></v-text-field>
+                                Sex<v-select solo dense :items="sex" v-model="user_details.sex"></v-select>
+                            </v-flex>
+                        </v-layout>
+                        <div>
+                            <v-btn class="primary red accent-4 mr-5" v-on:click="changeDetails">Change Details</v-btn>
+                        </div>
+                    </v-card-text>
+                </v-card>
+
+                 <v-card class="rounded-xl mt-12" elevation="5">
+                    <v-card-title>
+                        <span class="ml-5 mt-5">Change Password</span>
+                    </v-card-title>
+                    <v-card-text class="ml-7">
+                        <v-layout>
+                            <v-flex>
+                                Email<v-text-field dense solo v-model="user_password.email"  :rules="emailRules" :counter="30" required></v-text-field>
+                            </v-flex>
+                            <v-flex>
+                                Password<v-text-field dense solo v-model="user_password.password" :rules="[rules.required, rules.min]" counter hint="At least 8 characters" :append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'" :type="show_password ? 'text' : 'password'" @click:append="show_password = !show_password" required></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout>
+                            <v-flex>
+                                Confirm Password<v-text-field dense solo v-model="user_password.confirm_password" :rules="[rules.required, rules.min]" counter hint="At least 8 characters" :append-icon="confirmation_password ? 'mdi-eye' : 'mdi-eye-off'" :type="confirmation_password ? 'text' : 'password'" @click:append="confirmation_password = !confirmation_password" required></v-text-field>
+                            </v-flex>
+                            <v-flex>
+                                <v-btn class="primary mt-6 red accent-4" v-on:click="changePassword">Change Password</v-btn>
+                            </v-flex>
+                        </v-layout>
                     </v-card-text>
                 </v-card>
             </v-flex>
@@ -25,6 +62,9 @@
 .pop{
     font-family: 'Poppins', sans-serif;
 }
+.v-text-field{
+    width: 250px;
+}
 </style>
 
 <script>
@@ -35,7 +75,32 @@ axios.defaults.baseURL = 'http://localhost:8000'
 export default {
     data(){
         return{
-
+            sex: ['Female', 'Male'],
+            user_id:'',
+            user_password:'',
+            emailRules:[
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            rules: {
+                required: value => !!value || 'Required.',
+                min: v => v.length >= 8 || 'Min 8 characters',
+            },
+            user_details:{
+                first_name:'',
+                middle_name:'',
+                last_name:'',
+                extension_name:'',
+                address:'',
+                sex:''
+            },
+            user_password:{
+                email:'',
+                password:'',
+                confirm_password:''
+            },
+            show_password: false,
+            confirmation_password:false,
         }
     },
     mounted(){
@@ -44,9 +109,15 @@ export default {
     methods:{
         getUserData(){
             axios.get('/api/user').then(response =>{
-                this.userId = response.data.user_id
+                this.user_id = response.data.user_id
             })
         },
+        changeDetails(){
+            console.log(this.user_details.first_name + this.user_details.last_name + this.user_details.extension_name + this.user_details.address + this.user_details.sex)
+        },
+        changePassword(){
+            console.log(this.user_password.email + this.user_password.password + this.user_password.confirm_password)
+        }
     }
 }
 </script>
--- src/views/user/UserExamination.vue
+++ src/views/user/UserExamination.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pop mt-12 ml-12">
+    <div class="pop mt-5 ml-12">
         <h2 class="mb-12">Examination</h2>
         <v-layout>
             <v-flex>
@@ -49,7 +49,6 @@
                 </v-card>
             </v-flex>
         </v-layout>
-        
             <v-main>
                 <UserDashboard/>
             </v-main>
